0c66b24a63878a8bed3cbb60e42053b7288885c5
shimseongbo <seong265@icloud.com>
2024-01-01 10:17:12 +0900

    String, StringBuilder, StringBuffer

--- src/StringPerformance.java
+++ src/StringPerformance.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class StringPerformance {
+public class StringPerformance {
+
+    public static void main(String[] args) {
+        long startTime, endTime;
+
+        // String
+        String str = "";
+        startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 100000; i++) {
+            str += i; // 매번 새로운 String 객체 생성
+        }
+
+        endTime = System.currentTimeMillis();
+
+        System.out.println("Time taken by String: " + (endTime - startTime) + "ms");
+
+        // StringBuilder
+        StringBuilder stringBuilder = new StringBuilder();
+        startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 100000; i++) {
+            stringBuilder.append(i); // 같은 객체 내에서 변경
+        }
+
+        endTime = System.currentTimeMillis();
+        System.out.println("Time taken by StringBuilder: " + (endTime - startTime) + "ms");
+
+        // StringBuffer
+        StringBuffer stringBuffer = new StringBuffer();
+        startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 100000; i++) {
+            stringBuffer.append(i);
+        }
+
+        endTime = System.currentTimeMillis();
+        System.out.println("Time taken by StringBuffer: " + (endTime - startTime) + "ms");
+
+    }
 }
